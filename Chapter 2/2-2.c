0f479f14780e5c9d1c93c839c75992f1d41a9633
PaoRomancer <147646846+PaoRomancer@users.noreply.github.com>
2024-07-10 23:26:57 +0700

    Add files via upload

---
+++ Chapter 2/2-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("%-40s","Bangkok\n");
+    printf("%40s","Thailand\n");
+    printf("%1840s", "");
+    printf("%-40s", "King Mongkut's Institute of Technology Ladkrabang");
+    return 0;
+}
